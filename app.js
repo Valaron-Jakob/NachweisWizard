da310efc4c6598fb874c24af6511d63b670b453f
Jakob Humborg <jakob.humborg@unity.de>
2024-03-19 14:10:27 +0100

    Added azubi functionality

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mariadb from 'mariadb';
 import dotenv from 'dotenv';
-import db from './lib';
+import db from './lib.js';
 
 dotenv.config();
 
@@ -101,7 +101,7 @@ app.post("/ausbilder", async (request, response) => {
         }
 
         conn.beginTransaction();
-        results = await db.createAusbilder(conn, data); 
+        const results = await db.createAusbilder(conn, data); 
         conn.commit(); 
 
         return response.send({
@@ -147,6 +147,134 @@ app.delete("/ausbilder", async (request, response) => {
 
         response.send({
             "ausbilder_id": ausbilderId,
+            "user_id": userId
+        });
+
+    } catch (error) {
+        console.error(error);
+
+        if (conn) await conn.rollback();
+        response.status(500).send(error);
+
+    } finally {
+        if (conn) conn.end();
+    }
+});
+
+
+// Gibt alle Auszubildenden zurück wenn als Filter keine ID mitgegeben wurde
+app.get("/azubi", async (request, response) => {
+    let conn;
+
+    try {
+        conn = await pool.getConnection();
+
+        // Gibt einen Auszubildenden zurück wenn eine ID angegeben wurde
+        if (request.query.id) {
+            const azubiId = request.query.id;
+            const results = await db.getAzubi(conn, azubiId);
+
+            if (results.length === 0) {
+                return response.status(404).send({
+                    errors: ["There is no azubi with this id"]
+                });
+            }
+            return response.send(results);
+
+        // Gibt alle Auszubildenden zurück
+        } else {
+            const results = await db.getAllAzubi(conn);
+            return response.send(results);
+        }
+
+    } catch (error) {
+        console.log(error);
+        response.status(500).send(error);
+
+    } finally {
+        if (conn) conn.end();
+    }
+});
+
+// Legt einen neuen Auszubildenden an, wenn dieser noch nicht existiert
+app.post("/azubi", async (request, response) => {
+    let conn;
+
+    const data = request.body;
+
+    try {
+        conn = await pool.getConnection();
+
+        if (request.query.id) {
+            const azubiId = request.query.id;
+            const results = await db.editAzubi(conn, azubiId, data);
+
+            return response.send({
+                message: ["Edited azubi fields"],
+                azubi_id: azubiId,
+                data: data
+            });
+        }
+
+        // Überprüfen, ob ein Benutzer mit derselben E-Mail-Adresse bereits existiert
+        const userExists = await db.existsUserByMail(conn, data.email);
+
+        if (userExists) {
+            const user = await db.getUserByMail(conn, data.email);
+
+            return response.status(409).send({
+                errors: ["A user with this email already exists"],
+                user_id: user[0].user_id
+            });
+        }
+
+        conn.beginTransaction();
+        const results = await db.createAzubi(conn, data); 
+        conn.commit(); 
+
+        return response.send({
+            "azubi_id": Number(results[0].azubi_id)
+        });
+
+    } catch (error) {
+        console.log(error);
+
+        if (conn) await conn.rollback();
+        response.status(500).send(error);
+
+    } finally {
+        if (conn) conn.end();
+    }
+});
+
+// Löscht einen Auszubildenden, wenn dieser existiert
+app.delete("/azubi", async (request, response) => {
+    let conn;
+
+    try {
+        conn = await pool.getConnection();
+
+        const azubiId = request.query.id;
+        if (!azubiId) {
+            return response.status(400).send({
+                errors: ["An id parameter must be given"]
+            });
+        }
+
+        const azubiExists = await db.existsAzubi(conn, azubiId);
+        if (!azubiExists) {
+            return response.status(404).send();
+        }
+
+        await conn.beginTransaction();
+
+        const userId = await db.deleteAzubi(conn, azubiId);
+        await db.deleteUser(conn, userId);
+
+        await conn.commit();
+
+        response.send({
+            "azubi_id": azubiId,
             "user_id": userId
         });
 
--- lib.js
+++ lib.js
@@ -86,9 +86,9 @@ async function editAusbilder(conn, ausbilderId, data) {
     let queryString = [];
     let queryValues = [];
 
-    for (key in data) {
-        queryValues.push(data[key]);
+    for (const key in data) {
         queryString.push(key + " = ?");
+        queryValues.push(data[key]);
     }
 
     queryValues.push(ausbilderId);
@@ -126,6 +126,107 @@ async function deleteUser(conn, userId) {
     await conn.query("DELETE FROM an_user WHERE user_id = ?", [userId]);
 }
 
+
+/**
+ * Funktion zum Überprüfen, ob der Auszubildende existiert 
+ */ 
+async function existsAzubi(conn, azubiId) {
+    const results = await conn.query(
+        "SELECT azubi_id FROM auszubildender WHERE azubi_id = ?",
+        [azubiId]
+    );
+    return results.length > 0;
+}
+
+/**
+ * Funtion zum Zurückgeben aller Auszubildenden
+ */
+async function getAllAzubi(conn) {
+    return await conn.query(`
+        SELECT us.email, au.azubi_id
+        FROM an_user us
+        JOIN auszubildender au
+            ON us.user_id = au.user_id
+    `);
+}
+
+/**
+ * Funktion zum Zurückgeben eines Auszubildenden
+ */
+async function getAzubi(conn, azubiId) {
+    return await conn.query(`
+        SELECT us.user_id, us.vorname, us.nachname, us.email, us.abteilung, au.azubi_id, au.ausbilder_id, au.ausbildungsbeginn, au.ausbildungsberuf
+        FROM an_user us
+        JOIN auszubildender au
+            ON us.user_id = au.user_id
+        WHERE au.azubi_id = ?
+    `, [azubiId]);
+}
+
+/**
+ * Funktion zum Einfügen eines neuen Auszubildenden und Benutzers
+ */
+async function createAzubi(conn, data) {
+    const insertResults = await conn.query(`
+        INSERT INTO an_user (pw_hash, vorname, nachname, email, abteilung) VALUES (?, ?, ?, ?, ?);
+        SET @tmp_user_id = LAST_INSERT_ID();
+        
+        INSERT INTO auszubildender (user_id, ausbilder_id, ausbildungsbeginn, ausbildungsberuf) VALUES (@tmp_user_id, ?, ?, ?);
+        SET @tmp_azubi_id = LAST_INSERT_ID();
+        
+        SELECT @tmp_azubi_id AS azubi_id;
+    `, [
+        data.pw_hash.toString(),
+        data.vorname.toString(),
+        data.nachname.toString(),
+        data.email.toString(),
+        data.abteilung.toString(),
+        data.ausbilder_id.toString(),
+        data.ausbildungsbeginn.toString(),
+        data.ausbildungsberuf.toString()
+    ]);
+    console.log(insertResults)
+    return insertResults[4];
+}
+
+/**
+ * Funktion zum Bearbeiten eines bestehenden Ausbilders und Benutzers
+ */
+async function editAzubi(conn, azubiId, data) {
+    let queryString = [];
+    let queryValues = [];
+
+    for (const key in data) {
+        queryString.push(key + " = ?");
+        queryValues.push(data[key]);
+    }
+
+    queryValues.push(azubiId);
+
+    const results = await conn.query(`
+        UPDATE an_user us
+        JOIN auszubildender au
+            ON us.user_id = au.user_id
+        SET 
+            ${queryString.join(", ")}
+        WHERE au.azubi_id = ?;
+    `, queryValues);
+}
+
+/**
+ * Funktion zum Löschen des Auszubildenden
+ */
+async function deleteAzubi(conn, azubiId) {
+    const results = await conn.query(
+        "SELECT user_id FROM auszubildender WHERE azubi_id = ?",
+        [azubiId]
+    );
+
+    await conn.query("DELETE FROM auszubildender WHERE azubi_id = ?", [azubiId]);
+
+    return results[0].user_id;
+}
+
 export default {
     existsAusbilder,
     existsUserByMail,
@@ -135,5 +236,11 @@ export default {
     createAusbilder,
     editAusbilder,
     deleteAusbilder,
-    deleteUser
+    deleteUser,
+    existsAzubi,
+    getAllAzubi,
+    getAzubi,
+    createAzubi,
+    editAzubi,
+    deleteAzubi
 };
